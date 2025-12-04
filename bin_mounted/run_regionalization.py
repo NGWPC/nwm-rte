4bc5ed857c730a562639aee342381a9e52c602a4
Max Kipp <jkipp@dewberry.com>
2025-12-04 09:32:48 -0500

    Fix shell arg, fix secondary python venv reference, minor refactor

--- bin_mounted/run_regionalization.py
+++ bin_mounted/run_regionalization.py
@@ -9,7 +9,9 @@ from pydantic import validate_call
 
 print = functools.partial(print, flush=True)
 
-EVAL_VERF_PYTHON_ALIAS = "python_eval_verf"
+# EVAL_VERF_PYTHON_BINARY = "python_eval_verf"
+EVAL_VERF_PYTHON_BINARY = "/ngen-app/venvs/eval_verf/bin/python"
+
 REG_REPO_ROOT = "/ngen-app/nwm-region-mgr"
 REG_SAMPLE_CONFIGS = f"{REG_REPO_ROOT}/sample_files/configs"
 
@@ -20,7 +22,7 @@ def run_cmd(cmd: list[str], check: bool = True, cwd: str = None, shell: bool = F
         cmd = " ".join(cmd)
     msg = f"\n\n######## vvvvv\n{__file__}\nshell={shell}\ncwd={cwd}\nrunning={repr(cmd)}\n######## ^^^^^ \n"
     print(msg)
-    subprocess.run(cmd, check=check, cwd=cwd, shell=True)
+    subprocess.run(cmd, check=check, cwd=cwd, shell=shell)
 
 
 def main(parreg: bool, formreg: bool, ngen: bool, run_eval: bool):
@@ -29,7 +31,7 @@ def main(parreg: bool, formreg: bool, ngen: bool, run_eval: bool):
             cmd=[
                 "python",
                 f"{REG_REPO_ROOT}/regionalization.py",
-                f"{REG_SAMPLE_CONFIGS}",
+                REG_SAMPLE_CONFIGS,
             ],
             cwd=REG_REPO_ROOT,
         )
@@ -47,17 +49,17 @@ def main(parreg: bool, formreg: bool, ngen: bool, run_eval: bool):
         )
 
     if ngen:
+        cmd = [
+            "python",
+            f"{REG_REPO_ROOT}/run_ngen_vpu_docker.py",
+            "--config_ngen",
+            f"{REG_SAMPLE_CONFIGS}/config_ngen.yaml",
+        ]
+        # TODO remove the ulimit settings after ngen #90 has been merged
+        ulimit_extras = ["ulimit", "-n", "60000", "&&"]
+        cmd = ulimit_extras + cmd
         run_cmd(
-            cmd=[
-                "ulimit",
-                "-n",
-                "60000",
-                "&&",
-                "python",
-                f"{REG_REPO_ROOT}/run_ngen_vpu_docker.py",
-                "--config_ngen",
-                f"{REG_SAMPLE_CONFIGS}/config_ngen.yaml",
-            ],
+            cmd=cmd,
             cwd=REG_REPO_ROOT,
             shell=True,
         )
@@ -66,12 +68,13 @@ def main(parreg: bool, formreg: bool, ngen: bool, run_eval: bool):
         raise NotImplementedError("Not yet tested")
         run_cmd(
             cmd=[
-                EVAL_VERF_PYTHON_ALIAS,
+                EVAL_VERF_PYTHON_BINARY,
                 "-um",
                 "nwm.verf",
                 f"{REG_SAMPLE_CONFIGS}/config_eval.yaml",
             ],
             cwd=REG_REPO_ROOT,
+            shell=True,
         )
 
 
@@ -80,7 +83,8 @@ if __name__ == "__main__":
     parser.add_argument("--parreg", action="store_true")
     parser.add_argument("--formreg", action="store_true")
     parser.add_argument("--ngen", action="store_true")
-    parser.add_argument("--eval", dest="run_eval")  # eval is a python keyword so dest should be something else
+    # eval is a python keyword so dest should be something else
+    parser.add_argument("--eval", dest="run_eval", action="store_true")
     args = parser.parse_args()
     print(f"{__file__}: args: {args}")
 
