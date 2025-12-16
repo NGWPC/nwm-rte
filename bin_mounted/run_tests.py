9c0f318f6af426da5fce692688d39ccf7ec5480b
Max Kipp <jkipp@dewberry.com>
2025-12-16 17:17:17 -0500

    run_tests.py: add new Config pydantic class to manage CLI args and derived objects

--- bin_mounted/run_tests.py
+++ bin_mounted/run_tests.py
@@ -16,18 +16,76 @@ from execution_tests import (
     make_parallel_config,
 )
 from pseudocode import SavedState_Pseudo, StateManager_Pseudo
+from pydantic import BaseModel, ConfigDict, Field, validate_call
 
 import consts as c
 
 print = functools.partial(print, flush=True)
 
 
-def calibrations__build_and_run(test_paths: TestPaths, tests_manager: TestsManager, nprocs: int) -> None:
+class Config(BaseModel):
+    model_config = ConfigDict(strict=True, arbitrary_types_allowed=True)
+
+    delete_scratch_and_mesh_first: bool
+    delete_forcing_raw_input_first: bool
+    skip_forecast: bool
+    quit_forecast_after_forcing_running: bool
+    quit_forecast_after_duration: float | None = Field(ge=0)
+    do_calibration: bool
+    do_all_forcing_configs: bool
+    do_coldstart: bool
+    fcst_run_name: str
+    nprocs: int = Field(ge=1)
+    gage_id__gage_vintage: list[str] = Field(min_length=2, max_length=2)
+
+    # Set after init
+    gage_id: str = Field(init=False, default=None)
+    gage_vintage: str = Field(init=False, default=None)
+    tests_manager: TestsManager = Field(init=False, default=None)
+    state_manager: StateManager_Pseudo = Field(init=False, default=None)  # TODO pseudocode for now for states.
+    test_paths: TestPaths = Field(init=False, default=None)
+
+    def model_post_init(self, __context) -> None:
+        errors = []
+
+        gage_id, gage_vintage = self.gage_id__gage_vintage
+        if gage_id != gage_id.strip():
+            errors.append(ValueError(f"Whitespace found on end of gage_id: {repr(gage_id)}"))
+        if gage_vintage != gage_vintage.strip():
+            errors.append(ValueError(f"Whitespace found on end of gage_vintage: {repr(gage_vintage)}"))
+
+        if self.fcst_run_name != self.fcst_run_name.strip():
+            errors.append(ValueError(f"Whitespace found on end of fcst_run_name: {repr(self.fcst_run_name)}"))
+
+        if self.do_all_forcing_configs:
+            if self.skip_forecast and (not self.do_coldstart):
+                errors.append(
+                    ValueError(
+                        f"When do_all_forcing_configs={self.do_all_forcing_configs}, must have coldstart and/or forecast enabled."
+                    )
+                )
+
+        if errors:
+            raise RuntimeError(errors)
+
+        self.gage_id = gage_id
+        self.gage_vintage = gage_vintage
+        self.tests_manager = TestsManager()
+        self.state_manager = StateManager_Pseudo()
+        self.test_paths = TestPaths(
+            gage_id=gage_id,
+            gage_vintage=gage_vintage,
+            obj_func=c.CALIB_OBJECTIVE_FUNCTION,
+            optim_algo=c.CALIB_OPTIMIZATION_ALGO,
+        )
+
+
+def calibrations__build_and_run(cfg: Config) -> None:
     """Build calibration realizations and run them as tests."""
     for config_overrides in get_test_configs__calibration(
-        nprocs=nprocs,
-        gage_id=test_paths.gage_id,
-        gage_vintage=test_paths.gage_vintage,
+        nprocs=cfg.nprocs,
+        gage_id=cfg.gage_id,
+        gage_vintage=cfg.gage_vintage,
     ):
         fc = config_overrides.Forcing.forcing_configuration
         rb_kwargs = {"config_overrides": config_overrides}
@@ -43,27 +101,18 @@ def calibrations__build_and_run(test_paths: TestPaths, tests_manager: TestsManag
             print(f"### {fc}: executing calibration realization")
             t.execute_calibration()
 
-        tests_manager.add_forecast_test(t)
+        cfg.tests_manager.add_forecast_test(t)
 
 
-def forecasts__build_and_run(
-    test_paths: TestPaths,
-    tests_manager: TestsManager,
-    state_manager: StateManager_Pseudo,
-    do_all_forcing_configs: bool,
-    quit_forecast_after_forcing_running: bool,
-    quit_forecast_after_duration: float | None,
-    do_coldstart: bool,
-    fcst_run_name: str,
-    # nprocs: int,
-) -> None:
+def forecasts__build_and_run(cfg: Config, cs: bool) -> None:
     """
     Using ForecastTest, build and execute a list of forecast realizations.
     tests_manager is modified in-place, so some test results may be available if this function is interrupted.
+    `cs` controls whether coldstart is used (not `cfg.do_coldstart`).
     """
-    test_configs = get_test_configs__forecast(do_all_forcing_configs, do_coldstart)
+    test_configs = get_test_configs__forecast(cfg.do_all_forcing_configs, use_cold_start=cs)
     for tc in test_configs:
-        if quit_forecast_after_forcing_running and tc.Forcing.forcing_configuration != "short_range":
+        if cfg.quit_forecast_after_forcing_running and tc.Forcing.forcing_configuration != "short_range":
             raise NotImplementedError(
                 f"quit_forecast_after_forcing_running not yet tested for forcing_configuration = {repr(tc.Forcing.forcing_configuration)}"
             )
@@ -71,17 +120,17 @@ def forecasts__build_and_run(
     for config_overrides in test_configs:
         fc = config_overrides.Forcing.forcing_configuration
         rb_kwargs = {
-            # "input_path": test_paths.dir_input,
-            "valid_yaml": test_paths.valid_yaml,
-            "fcst_run_name": fcst_run_name,
+            # "input_path": cfg.test_paths.dir_input,
+            "valid_yaml": cfg.test_paths.valid_yaml,
+            "fcst_run_name": cfg.fcst_run_name,
             "config_overrides": config_overrides,
-            "use_cold_start": do_coldstart,
+            "use_cold_start": cs,
         }
         print(f"\n\n##########\n### {fc}: setting up test with rb_kwargs = {rb_kwargs}")
 
         t = ForecastTest(
             rb_kwargs=rb_kwargs,
-            ngen_log=LogParser(path=f"{test_paths.dir_output}/Forecast_Run/{fcst_run_name}/logs/ngen.log"),
+            ngen_log=LogParser(path=f"{cfg.test_paths.dir_output}/Forecast_Run/{cfg.fcst_run_name}/logs/ngen.log"),
         )
 
         # Build the realization, trapping exceptions into class attrs
@@ -92,89 +141,44 @@ def forecasts__build_and_run(
             # Execute the realization via ngen, trapping exceptions and logs into class attrs
             print(f"### {fc}: executing realization via ngen")
             t.execute_forecast(
-                quit_forecast_after_forcing_running=quit_forecast_after_forcing_running,
-                quit_forecast_after_duration=quit_forecast_after_duration,
+                quit_forecast_after_forcing_running=cfg.quit_forecast_after_forcing_running,
+                quit_forecast_after_duration=cfg.quit_forecast_after_duration,
             )
 
             if t.rb.input_configs_class.Forcing.forcing_configuration == "standard_ana":
-                state_manager.add_saved_state(
+                cfg.state_manager.add_saved_state(
                     SavedState_Pseudo(
                         dt=datetime.strptime(t.rb.input_configs_class.Forcing.cycle_datetime, DEFAULT_DATETIME_FORMAT),
                         realization_file=t.rb.realization_file,
                     )
                 )
 
-        tests_manager.add_forecast_test(t)
+        cfg.tests_manager.add_forecast_test(t)
 
 
-def main(
-    delete_scratch_and_mesh_first: bool,
-    delete_forcing_raw_input_first: bool,
-    skip_forecast: bool,
-    quit_forecast_after_forcing_running: bool,
-    quit_forecast_after_duration: float | None,
-    do_calibration: bool,
-    do_all_forcing_configs: bool,
-    do_coldstart: bool,
-    fcst_run_name: str,
-    nprocs: int,
-    gage_id__gage_vintage: list[str],
-):
-
-    if not fcst_run_name.strip():
-        raise ValueError(f"Empty fcst_run_name: {repr(fcst_run_name)}")
-    if do_all_forcing_configs:
-        if not (do_coldstart or (not skip_forecast)):
-            raise ValueError(
-                f"When do_all_forcing_configs={do_all_forcing_configs}, must have coldstart and/or forecast enabled."
-            )
-
-    gage_id, gage_vintage = gage_id__gage_vintage
-    test_paths = TestPaths(
-        gage_id=gage_id,
-        gage_vintage=gage_vintage,
-        obj_func=c.CALIB_OBJECTIVE_FUNCTION,
-        optim_algo=c.CALIB_OPTIMIZATION_ALGO,
-    )
-
-    utils_testing_setup.assert_paths__core(test_paths)
-    # utils_testing_setup.assert_paths__raw_config(test_paths)  # Only works for default gage
-
-    # TODO pseudocode for now for states.
-    state_manager = StateManager_Pseudo()
-    tests_manager = TestsManager()
-
+@validate_call
+def main(cfg: Config):
+    utils_testing_setup.assert_paths__core(cfg.test_paths)
+    # utils_testing_setup.assert_paths__raw_config(cfg.test_paths)  # Only works for default gage
     ### NOTE this deletes the test output dir.
     ### If wanting to skip Calibration but still do CS and/or Forecast,
     ### then remove this line so that the test calibration results remain available.
-    # utils_testing_setup.delete_test_output_dir(test_paths)
+    # utils_testing_setup.delete_test_output_dir(cfg.test_paths)
 
-    if delete_scratch_and_mesh_first:
-        utils_testing_setup.delete_scratch_and_esmf_outputs(test_paths)
-    if delete_forcing_raw_input_first:
+    if cfg.delete_scratch_and_mesh_first:
+        utils_testing_setup.delete_scratch_and_esmf_outputs(cfg.test_paths)
+    if cfg.delete_forcing_raw_input_first:
         utils_testing_setup.delete_forcing_raw_inputs()
 
-    if do_calibration:
-        calibrations__build_and_run(test_paths, tests_manager, nprocs)
+    if cfg.do_calibration:
+        calibrations__build_and_run(cfg)
 
-    forecast_kwargs_base = {
-        "test_paths": test_paths,
-        "tests_manager": tests_manager,
-        "state_manager": state_manager,
-        "do_all_forcing_configs": do_all_forcing_configs,
-        "quit_forecast_after_forcing_running": quit_forecast_after_forcing_running,
-        "quit_forecast_after_duration": quit_forecast_after_duration,
-        "fcst_run_name": fcst_run_name,
-        # "nprocs": nprocs,
-    }
+    if cfg.do_coldstart:
+        forecasts__build_and_run(cfg, cs=True)
+    if not cfg.skip_forecast:
+        forecasts__build_and_run(cfg, cs=False)
 
-    if do_coldstart:
-        forecasts__build_and_run(**(forecast_kwargs_base | {"do_coldstart": True}))
-
-    if not skip_forecast:
-        forecasts__build_and_run(**(forecast_kwargs_base | {"do_coldstart": False}))
-
-    tests_manager.evaluate_test_results()
+    cfg.tests_manager.evaluate_test_results()
 
 
 if __name__ == "__main__":
@@ -255,4 +259,6 @@ When nprocs > 1, Calibration's ParallelConfig is like: {make_parallel_config(npr
     )
     args = parser.parse_args()
     print(f"args: {args}")
-    main(**vars(args))
+
+    cfg = Config(**vars(args))
+    main(cfg)
