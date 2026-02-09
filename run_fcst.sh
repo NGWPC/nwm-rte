62d1b42a329db2769aa9f4514d4d20660b424904
Max Kipp <jkipp@dewberry.com>
2026-02-08 20:50:20 -0500

    Expand run_fcst.sh

--- run_fcst.sh
+++ run_fcst.sh
@@ -11,7 +11,9 @@ set -x
 
 
 # docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" --help
-docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" -dt "2025-09-15 00:00:00" -rname "${fcst_run_name}"
-# docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" -dt "2025-09-15 00:00:00" -csdt "2025-09-12 00:00:00" -rname "${fcst_run_name}"
+# docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" -csdt "2025-09-12 00:00:00" -dt "2025-09-15 00:00:00" -rname "${fcst_run_name}_cs_short" -fconfig short_range
+docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" -dt "2025-09-15 00:00:00" -rname "${fcst_run_name}_short" -fconfig short_range
+# docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" -dt "2025-09-15 00:00:00" -rname "${fcst_run_name}_ana" -fconfig standard_ana
+# docker_run python "/ngen-app/bin/bin_mounted/run_forecast.py" -dt "2025-09-15 00:00:00" -rname "${fcst_run_name}_mr" -fconfig medium_range
 
 exit 0
