5a96792bf816254ba3a9907329f20ce9dfcf56da
Max Kipp <jkipp@dewberry.com>
2026-04-07 21:38:03 -0400

    Add pytest for building and running default realization

---
+++ bin_mounted/conftest.py
@@ -0,0 +1,7 @@
+import os
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_env():
+    os.environ["NGEN_LOG_TO_RTE"] = "true"
---
+++ bin_mounted/test_realizations.py
@@ -0,0 +1,58 @@
+"""Tests for building and running ngen realizations."""
+
+import logging
+
+from mswm.build_inputs import RealizationBuilder
+
+from bin_mounted import run_default
+from bin_mounted import configs
+
+
+# def test_build_default_realization():
+#     _, _ = _build_default_realization()
+
+
+def test_run_default_realization():
+    rb, cfg = _build_default_realization()
+    output_ngen_stdout_stderr_log = run_default.run_default(
+        rb, cfg, clear_output_dir=True
+    )
+
+    logging.info(f"Reading: {output_ngen_stdout_stderr_log}")
+    with open(output_ngen_stdout_stderr_log) as f:
+        log_lines = f.read().splitlines()
+
+    critical_lines = [x for x in log_lines if "CRITICAL" in x]
+    fatal_lines = [x for x in log_lines if "FATAL" in x]
+
+    if critical_lines or fatal_lines:
+        msg = f"{len(critical_lines)} critical lines and {len(fatal_lines)} fatal lines found in: {repr(output_ngen_stdout_stderr_log)}"
+        logging.critical(f"critical lines: {critical_lines}")
+        logging.critical(f"fatal lines: {fatal_lines}")
+        raise RuntimeError(msg)
+
+
+def _build_default_config() -> configs.RTEDefaultConfig:
+    parser = run_default.cli_arg_parser()
+    args = parser.parse_args(
+        [
+            "--forcing_configuration",
+            "aorc",
+            "--cycle_datetime",
+            "2013-07-25 00:00:00",
+            "--historical_sim_duration",
+            "2",
+            "--fcst_run_name",
+            "test_default_aorc",
+            "--nprocs",
+            "2",
+        ]
+    )
+    cfg = configs.RTEDefaultConfig(**vars(args))
+    return cfg
+
+
+def _build_default_realization() -> tuple[RealizationBuilder, configs.RTEDefaultConfig]:
+    cfg = _build_default_config()
+    rb = run_default.build_default_realization(cfg)
+    return rb, cfg
