63cf61fe692ab22a8f556fc51344a9bf6b44b4f1
Max Kipp <jkipp@dewberry.com>
2026-04-13 22:12:18 -0400

    Clean up and document devcontainer.json. Add poststart shell script for pip (re)installs

---
+++ .devcontainer/devcontainer.json.poststart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+# python -m pip install -e ./src/ngen-forcing
+# python -m pip install -e ./src/nwm-fcst-mgr
+# python -m pip install -e ./src/nwm-msw-mgr
+# python -m pip install -e ./src/nwm-cal-mgr
+
+exit 0
