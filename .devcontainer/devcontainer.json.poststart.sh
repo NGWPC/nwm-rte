12eaf81b81fa516d0af3de9eacd5163e530f8e35
Max Kipp <jkipp@dewberry.com>
2026-04-14 17:51:22 -0400

    Dev Container: add mount for ngen source code, add placeholder for reinstall of local changes to lstm

--- .devcontainer/devcontainer.json.poststart.sh
+++ .devcontainer/devcontainer.json.poststart.sh
@@ -3,7 +3,13 @@
 set -euo pipefail
 set -x
 
+### Reinstall a specific ngen extern submodule
+# python -m pip install -e ./src/ngen/extern/lstm
+
+### Reinstall forcing (Python package only)
 # python -m pip install -e ./src/ngen-forcing
+
+### Reinstall various components
 # python -m pip install -e ./src/nwm-fcst-mgr
 # python -m pip install -e ./src/nwm-msw-mgr
 # python -m pip install -e ./src/nwm-cal-mgr
