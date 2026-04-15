5652000bb1d3404c8ee7e1c18d53737a53cb3bd9
Max Kipp <jkipp@dewberry.com>
2026-04-15 16:48:18 -0400

    Add t-route and topoflow-glacier to devcontainer poststart (re)install script

--- .devcontainer/devcontainer.json.poststart.sh
+++ .devcontainer/devcontainer.json.poststart.sh
@@ -5,6 +5,8 @@ set -x
 
 ### Reinstall a specific ngen extern submodule
 # python -m pip install -e ./src/ngen/extern/lstm
+# python -m pip install -e ./src/ngen/extern/t-route
+# python -m pip install -e ./src/ngen/extern/topoflow-glacier
 
 ### Reinstall forcing (Python package only)
 # python -m pip install -e ./src/ngen-forcing
