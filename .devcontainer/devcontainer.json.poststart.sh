d48e923f0a21b658688d57612db1136a2bb3b8ec
Max Kipp <jkipp@dewberry.com>
2026-05-29 21:45:47 -0400

    Update poststart.sh to handle new approach of how forcing config files are installed

--- .devcontainer/devcontainer.json.poststart.sh
+++ .devcontainer/devcontainer.json.poststart.sh
@@ -8,8 +8,10 @@ set -x
 # python -m pip install -e ./src/ngen/extern/t-route
 # python -m pip install -e ./src/ngen/extern/topoflow-glacier
 
-### Reinstall forcing (Python package only)
+### Reinstall forcing (Python package only).
+### If doing this with -e, also need the symlink below since -e uninstalls package data and does not reinstall it.
 # python -m pip install -e ./src/ngen-forcing
+# ln -s $(pwd)/src/ngen-forcing/NextGen_Forcings_Engine_BMI $(python -c "import site; print(site.getsitepackages()[0])")/NextGen_Forcings_Engine_BMI
 
 ### Reinstall nwm-ewts (Python package only)
 # python -m pip install -e ./src/nwm-ewts/runtime/python/ewts
