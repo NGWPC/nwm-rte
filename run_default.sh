2830d1ea6ab85c0861ae6a32388caed5cdd1f2bb
jeff.wade <jeff.s.wade@rtx.com>
2026-07-23 14:47:52 -0400

    Update edfs da runs to desired gage and times

--- run_default.sh
+++ run_default.sh
@@ -28,63 +28,19 @@ set -x
 
 
 # 7 day Cold Start
-docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-09-14 00:00:00" -dt "2025-09-15 00:00:00" -rname "default_ana_7day" -fconfig short_range --save_state
+docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2023-12-10 00:00:00" -dt "2025-12-17 00:00:00" -rname "default_ana_7day" -fconfig short_range --save_state -v "01" --output_format NetCDF
 
 # 1 day Intermediate AnAs
-docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-09-15 00:00:00" -dt "2025-09-16 00:00:00" -rname "default_ana_-2d_-1d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_7day/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-09-16 00:00:00" -dt "2025-09-17 00:00:00" -rname "default_ana_-1d_0d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_-2d_-1d/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-09-17 00:00:00" -dt "2025-09-18 00:00:00" -rname "default_ana_0d_1d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_-1d_0d/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-09-18 00:00:00" -dt "2025-09-19 00:00:00" -rname "default_ana_1d_2d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_0d_1d/01123000/state_save/
+docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-12-17 00:00:00" -dt "2025-12-18 00:00:00" -rname "default_ana_-2d_-1d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_7day/01123000/state_save/ -v "01" --output_format NetCDF
+docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-12-18 00:00:00" -dt "2025-12-19 00:00:00" -rname "default_ana_-1d_0d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_-2d_-1d/01123000/state_save/ -v "01" --output_format NetCDF 
+docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-12-19 00:00:00" -dt "2025-12-20 00:00:00" -rname "default_ana_0d_1d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_-1d_0d/01123000/state_save/ -v "01" --output_format NetCDF
+docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-12-20 00:00:00" -dt "2025-12-21 00:00:00" -rname "default_ana_1d_2d" -fconfig short_range --save_state --load_state_from /ngwpc/run_ngen/default/default_ana_0d_1d/01123000/state_save/ -v "01" --output_format NetCDF
 
 # 5 Short Range
-docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-09-15 00:00:00" -rname "default_sr_-2d" --load_state_from /ngwpc/run_ngen/default/default_ana_7day/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-09-16 00:00:00" -rname "default_sr_-1d" --load_state_from /ngwpc/run_ngen/default/default_ana_-2d_-1d/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-09-17 00:00:00" -rname "default_sr_0d" --load_state_from /ngwpc/run_ngen/default/default_ana_-1d_0d/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-09-18 00:00:00" -rname "default_sr_1d" --load_state_from /ngwpc/run_ngen/default/default_ana_0d_1d/01123000/state_save/
-docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-09-19 00:00:00" -rname "default_sr_2d" --load_state_from /ngwpc/run_ngen/default/default_ana_1d_2d/01123000/state_save/
-
-# # Short Range
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-03-30 06:00:00" -rname "default_sr"
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-03-30 06:00:00" -rname "default_sr" -g "12175500"
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-03-30 06:00:00" -rname "default_sr" -v "03S"
-
-# # Cold Start
-# docker_run python -um "ngen_rte.run_default" -n 2 -csdt "2025-09-14 00:00:00" -dt "2025-09-15 00:00:00" -rname "default_cs_short" -fconfig short_range --save_state
-
-# # Short Range with NWM Output Variables
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-03-30 06:00:00" -rname "default_sr_nwm_output" -nwmout --output_format CSV NetCDF
-# # Analysis & Assimilation
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "standard_ana" -dt "2026-03-30 06:00:00" -rname "default_ana"
-# # Medium Range
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range_blend" -dt "2026-03-30 06:00:00" -rname "default_mr"
-
-# # Medium Range Lagged Ensemble
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "no_da" "" ""
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "mem1" "" ""
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "mem2" "" ""
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "mem3" "" ""
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "mem4" "" ""
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "mem5" "" ""
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "medium_range" -dt "2026-03-30 06:00:00" -rname "default_mr_le" -le "mem6" "" ""
-
-# # # Historical / Retrospective Forcing
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "aorc" -dt "2013-07-25 00:00:00" -dur 2 -rname "default_aorc"
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "aorc" -dt "2013-07-25 00:00:00" -dur 2 -rname "default_aorc" -mf "snow-17,noah-owp-modular,smp,lasam,t-route" -rz "true"
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "aorc" -dt "2013-07-25 00:00:00" -dur 2 -rname "default_aorc" --output_format CSV NetCDF
-
-# # Reservoir RFC Data Assimilation (RFC reservoir test data only valid at "2026-07-13 12:00:00")
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-07-13 12:00:00" -rname "default_sr" -g "01205500" -rfc /ngwpc/run_ngen/data/rfc/reservoirs/timeseries/
-# # docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-07-13 12:00:00" -rname "default_sr_no_da" -g "01205500"
-
-# # State saving and loading
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "standard_ana" -dt "2026-03-30 06:00:00" -rname "default_ana" --save_state --save_state_dir /ngwpc/run_ngen/default/default_ana/${TEST_GAGE}/state_save_directory/
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "standard_ana" -dt "2026-03-30 06:00:00" -rname "default_ana" --save_state
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-03-30 06:00:00" -rname "default_sr" --load_state_from /ngwpc/run_ngen/default/test_bmi/${TEST_GAGE}/state_save/
-
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "aorc" -dt "2013-07-25 00:00:00" -dur 2 -rname "default_aorc" --gage_id ${TEST_GAGE} --hydrofab_file "${TEST_HYDROFAB_FILE}"
-
-# # Checkpoint restart
-# docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2026-03-30 06:00:00" -rname "default_sr" --checkpoint_interval 3 --checkpoint_dir "/ngwpc/run_ngen/default/default_sr/${TEST_GAGE}/checkpoint_directory/" -nwmout --output_format NetCDF
-# docker_run python -um "ngen_rte.run_restart" -src "/ngwpc/run_ngen/default/default_sr/${TEST_GAGE}/" -dst "/ngwpc/run_ngen/default/default_sr_restart/${TEST_GAGE}/" --checkpoint_dir "/ngwpc/run_ngen/default/default_sr/${TEST_GAGE}/checkpoint_directory/"
+docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-12-17 00:00:00" -rname "default_sr_-2d" --load_state_from /ngwpc/run_ngen/default/default_ana_7day/01123000/state_save/ -v "01" --output_format NetCDF
+docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-12-18 00:00:00" -rname "default_sr_-1d" --load_state_from /ngwpc/run_ngen/default/default_ana_-2d_-1d/01123000/state_save/ -v "01" --output_format NetCDF
+docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-12-19 00:00:00" -rname "default_sr_0d" --load_state_from /ngwpc/run_ngen/default/default_ana_-1d_0d/01123000/state_save/ -v "01" --output_format NetCDF
+docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-12-20 00:00:00" -rname "default_sr_1d" --load_state_from /ngwpc/run_ngen/default/default_ana_0d_1d/01123000/state_save/ -v "01" --output_format NetCDF
+docker_run python -um "ngen_rte.run_default" -n 2 -fconfig "short_range" -dt "2025-12-21 00:00:00" -rname "default_sr_2d" --load_state_from /ngwpc/run_ngen/default/default_ana_1d_2d/01123000/state_save/ -v "01" --output_format NetCDF
 
 exit 0
